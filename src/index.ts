c48c3cd96cd4107d9a3c328b4231df4e12b91906
Josh Goldberg <josh@fullscreenmario.com>
2017-07-25 14:13:43 -0700

    Added basic package level exports to src/index.ts

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+export { GlsLine } from "./glsLine";
+export { ITransformer, TransformationService } from "./service";
+export { IOutput, IRange, Transformation } from "./transformation";
+export * from "./transforms";
