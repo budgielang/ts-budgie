d01a18e140c06f3608d2026b19ec41711dcbc1d2
Josh Goldberg <josh@fullscreenmario.com>
2018-08-20 04:09:47 -0700

    Added 'in' support to binary expressions

--- src/nodes/visitors/binaryExpressionVisitor.ts
+++ src/nodes/visitors/binaryExpressionVisitor.ts
@@ -1,5 +1,5 @@
 import { CommandNames } from "general-language-syntax";
-import { BinaryExpression, Expression, isBinaryExpression, SyntaxKind } from "typescript";
+import * as ts from "typescript";
 
 import { UnsupportedComplaint } from "../../output/complaint";
 import { GlsLine } from "../../output/glsLine";
@@ -8,43 +8,41 @@ import { operators } from "../../parsing/aliases";
 import { filterOutUnsupportedComplaint } from "../../utils";
 import { NodeVisitor } from "../visitor";
 
-const collectOperationContents = (node: BinaryExpression): (string | Expression)[] => {
-    const contents: (string | Expression)[] = [];
-    const { left, right } = node;
-
-    if (isBinaryExpression(left)) {
-        contents.push(...collectOperationContents(left));
-    } else {
-        contents.push(left);
-    }
-
-    contents.push((operators as { [i: number]: string })[node.operatorToken.kind]);
-
-    if (isBinaryExpression(right)) {
-        contents.push(...collectOperationContents(right));
-    } else {
-        contents.push(right);
-    }
-
-    return contents;
-};
+const unknownOperatorComplaint = "Unknown operator kind.";
 
 export class BinaryExpressionVisitor extends NodeVisitor {
-    public visit(node: BinaryExpression) {
-        if (node.operatorToken.kind === SyntaxKind.InstanceOfKeyword) {
+    public visit(node: ts.BinaryExpression) {
+        if (node.operatorToken.kind === ts.SyntaxKind.InstanceOfKeyword) {
             return [
                 Transformation.fromNode(
                     node,
                     this.sourceFile,
                     [
-                        new GlsLine(CommandNames.InstanceOf, node.left.getText(this.sourceFile), node.right.getText(this.sourceFile)),
+                        new GlsLine(
+                            CommandNames.InstanceOf,
+                            node.left.getText(this.sourceFile),
+                            node.right.getText(this.sourceFile)),
+                    ]),
+            ];
+        }
+
+        if (node.operatorToken.kind === ts.SyntaxKind.InKeyword) {
+            return [
+                Transformation.fromNode(
+                    node,
+                    this.sourceFile,
+                    [
+                        new GlsLine(
+                            CommandNames.DictionaryContainsKey,
+                            node.left.getText(this.sourceFile),
+                            node.right.getText(this.sourceFile)),
                     ]),
             ];
         }
 
         const contents = filterOutUnsupportedComplaint(
-                collectOperationContents(node)
-                    .map((content) => this.recurseOnOperationContents(content)));
+            this.collectOperationContents(node)
+                .map((content) => this.recurseOnOperationContents(content)));
         if (contents instanceof UnsupportedComplaint) {
             return contents;
         }
@@ -59,8 +57,33 @@ export class BinaryExpressionVisitor extends NodeVisitor {
         ];
     }
 
-    private recurseOnOperationContents(content: string | Expression) {
-        return typeof content === "string"
+    private collectOperationContents(node: ts.BinaryExpression): (string | ts.Expression | UnsupportedComplaint)[] {
+        const contents: (string | ts.Expression | UnsupportedComplaint)[] = [];
+        const { left, right } = node;
+
+        if (ts.isBinaryExpression(left)) {
+            contents.push(...this.collectOperationContents(left));
+        } else {
+            contents.push(left);
+        }
+
+        if (node.operatorToken.kind in operators) {
+            contents.push((operators as { [i: number]: string })[node.operatorToken.kind]);
+        } else {
+            contents.push(UnsupportedComplaint.forNode(node, this.sourceFile, unknownOperatorComplaint));
+        }
+
+        if (ts.isBinaryExpression(right)) {
+            contents.push(...this.collectOperationContents(right));
+        } else {
+            contents.push(right);
+        }
+
+        return contents;
+    }
+
+    private recurseOnOperationContents(content: string | ts.Expression | UnsupportedComplaint) {
+        return typeof content === "string" || content instanceof UnsupportedComplaint
             ? content
             : this.router.recurseIntoValue(content);
     }
